04e12e3f8e592f0dbce1559bdc442b18a11a9cc4
Collino <coadhola@gmail.com>
2021-06-03 10:39:19 +0100

    Adding more to templates

---
+++ src/todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import new_todo, update_todo, delete_todo, list_todo
+
+app_name = 'todo'
+
+
+urlpatterns = [
+  path('', list_todo, name="list-todo"),
+  path('new-todo', new_todo, name="new-todo"),
+  path('update-todo/<int:pk>', update_todo, name="update-todo"),
+  path('delete-todo/<int:pk>', delete_todo, name="delete-todo"),  
+    ]
---
+++ src/todo/views.py
@@ -0,0 +1,43 @@
+
+# pyflaxes.ignore from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import TodoForm
+
+from .models import Todo
+
+
+# Function based views
+
+def list_todo(request):
+  todos = Todo.objects.all()
+  return render(request, 'list_todo.html', context={"todos": todos})
+
+
+def new_todo(request):
+  form = TodoForm(request.POST or None)
+  if form.is_valid():
+    form.save()
+    return redirect('todo:list-todo')
+  return render(request, 'new_todo.html', context={"form":form}) 
+
+
+def update_todo(request, pk):
+  todo = get_object_or_404(Todo, pk=pk)
+  form = TodoForm(request.POST or None, instance=todo)
+
+  if form.is_valid():
+    form.save()
+    return redirect('todo:list-todo')
+
+  return render(request, 'update_todo.html', context={'form':form})
+
+
+def delete_todo(request, pk):
+  todo = get_object_or_404(Todo, pk=pk)
+
+  if request.method == 'POST':
+    todo.delete()
+    return redirect('todo:list-todo')
+  return render(request, 'delete_todo.html', context={})  
+
